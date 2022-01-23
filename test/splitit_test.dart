fe70133276c78f27b90f4df42a63cc744c8fa307
Ifeanyi Agu <aaifeanyi20@outlook.com>
2022-01-23 16:19:52 +0100

    maintenance

--- test/splitit_test.dart
+++ test/splitit_test.dart
@@ -5,12 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:splitip/Screens/homepage.dart';
 import 'package:splitip/answermodel.dart';
 
-import 'package:splitip/main.dart';
 
 void main() {
   test('To calculate the tips and splits', () {
